d8990c281b405925799630598b4e44b9f9875ca2
Andrew-Morris <blatadude@gmail.com>
2017-10-02 16:59:41 -0700

    Massive changes. Actually works now for key mapping

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const fetch = require('node-fetch');
 const merge = require('deep-extend')
 const deepKeys = require('deep-keys')
-const deepFind = require('deep-find')
+const deepFind = require('get-deep')
 const detectType = require('type-detect')
 const _ = require('lodash')
-// Big thanks to DavDavDavid for the fetch error handler and JSON parser
+// Credit to DavDavDavid for the fetch error handler and JSON parser
 const benignErrorCodes = [
     'warming_up'
   ]
 const makeFetchUsers = (fetcher) => {
     const fetchUsers = async (onFailWait=5000) => {
-      let data = await fetcher()
+      const data = await fetcher()
       if (data.error_code) {
         console.error(data.error_message)
         if (benignErrorCodes.includes(data.error_code)) {
@@ -23,7 +23,7 @@ const makeFetchUsers = (fetcher) => {
     return fetchUsers
 }
 
-const getKeys = async () => {
+const getUsers = async () => {
     const fetchUsers = makeFetchUsers(async () =>
     await (await fetch('https://ffforumautomator.herokuapp.com/hackable-data')).json()
     )
@@ -32,89 +32,43 @@ const getKeys = async () => {
     const parsedObj = data.map(user => {
         return parseHackableJSON(user.hackable_json)
     })
-    // Some user managed to stringify his json so that it retained type === string after JSON parsing
-    const objects = parsedObj.filter(item => {
+    // Some user thought they needed to use JSON string instead of a POJO
+    return parsedObj.filter(item => {
         return typeof(item) === "object"
     })
-    // console.log(objects)
-    // Merge objects into one
-    const merged = merge(...objects)
-    // merged
-    // Get just the keys
-    const keys = deepKeys(merged)
-    // console.log(keys)
-    let objectCount = 0
-    let stringCount= 0
-    let arrayCount = 0
-    let numberCount = 0
-    let undefinedCount = 0
-    let booleanCount = 0
-    let otherTypes = []
-    // for (let keyList of keys) {
-    //   const keyNesting = keyList.split('.')
-
-
-    //   // for all possible paths, go to one (level) log the type,(iterated is obj, then iterated[level] is next , log the type, repeat
-    //   objects.map(obj => {
-    //     // console.log(iterated)
-    //     let iterated = obj
-    //     for (let level of keyNesting) {
-    //       iterated[level] ? iterated = iterated[level] : iterated = obj
-    //       if (typeof(iterated) === 'string') {
-    //         console.log(iterated)
-    //         stringCount++
-    //       }
-    //       else if (Array.isArray(iterated)) {
-    //         console.log(iterated)
-    //         arrayCount++
-    //       }
-    //       else if (typeof(iterated) === 'object') objectCount++
-    //       else if (typeof(iterated) === 'number') numberCount++
-    //       else if (typeof(iterated) === 'undefined') undefinedCount++
-    //       else if (typeof(iterated) === 'boolean') booleanCount++ 
-    //     }
-    //     console.log('deep? ', iterated)
-    //   })
-        
-    //     // console.log(iterated)
-      
-    // }
-    // const parsed = keys.map(key => key.split('.'))
-    // console.log(keys)
-    // console.log(parsed)
-    
-    
-    
-    // console.log(split)
-    // console.log(merged)
-    const p = objects.map(object => {
-      // console.log(object)
-      return keys.map(key => {
-        try {
-          return detectType(deepFind(object, key))
-        } catch (err) {
-        }  
-      })
-      
-    })
-    const little = p.map(p => {
-      // const flat = _.flatten(p)
-      // remove nonexistent things
-      return p.filter(item => item != undefined && item != 'undefined')
-    })
-    console.log(little)
-    
-    // const types = _.compact(_.flattenDeep(p))
-    // console.log(types)
-
-
-}
-const parseHackableJSON = (hackableJSON) => {
-    try {
-      return JSON.parse(hackableJSON)
-    } catch (err) {
-      return undefined
-    }
   }
-getKeys().then(console.log)
 
+const findKeyType = (users) => {
+  // Merge users into one object for easier handling
+  const merged = merge(...users)
+  // Key Array
+  const keys = deepKeys(merged)
+
+  // recursively check all the keys for each user, return if value is truthy
+  const pairs = users.map(user => {  
+    const deep = keys.map(key => {
+      const found = deepFind(user, key)
+      // not sure if should be obj or arr hmmm
+      if (found) return {
+        key,
+        value: found,
+        type: detectType(found)
+      }
+    })
+      // map returns undefined if no return, so they get removed here
+      return deep.filter(item => (item))    
+  })
+  return pairs
+}
+
+const parseHackableJSON = (hackableJSON) => {
+  try {
+    return JSON.parse(hackableJSON)
+  } catch (err) {
+    return undefined
+  }
+}
+
+getUsers().then(users => {
+  console.log(findKeyType(users))
+})
